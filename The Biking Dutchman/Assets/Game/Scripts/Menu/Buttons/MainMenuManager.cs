cea171803ecbc3c7a0e511b67df6b1cdaf2a0f5d
Troy2000 <troy_toendang@hotmail.com>
2020-03-11 10:40:38 +0100

    Implemented Basic menu

---
+++ The Biking Dutchman/Assets/Game/Scripts/Menu/Buttons/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject optionsPanel;
+
+    void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    // Executes when 'Play' clicked
+    public void PlayGame()
+    {
+        Debug.Log("Play Game");
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    // Executes when 'Options' clicked
+    public void ShowOptions()
+    {
+        Debug.Log("Show Options");
+        optionsPanel.SetActive(true);
+    }
+
+    public void HideOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    // Executes when 'Quit' clicked
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+}
