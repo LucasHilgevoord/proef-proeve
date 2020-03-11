21e2c425b13d2341e55547ecae58a0c5e889bcc8
LucasLaptop <lucashilgevoordd@gmail.com>
2020-03-11 13:05:49 +0100

    Created basic camera following

---
+++ The Biking Dutchman/Assets/Game/Scripts/Entities/Player/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+    private Vector3 offset;
+    private float followSpeed = 0.1f;
+
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 newPos = Vector3.Lerp(transform.position, desiredPos, followSpeed);
+        transform.position = newPos;
+    }
+}
