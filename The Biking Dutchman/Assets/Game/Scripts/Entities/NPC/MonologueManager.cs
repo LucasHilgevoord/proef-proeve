583cace54b05210cd2f74d581ccfd00aa9ef489d
maxhdekker <xamrekked@gmail.com>
2020-03-12 09:55:07 +0100

    New NPC Scene

---
+++ The Biking Dutchman/Assets/Game/Scripts/Entities/NPC/MonologueManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonologueManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject npcMessage;
+
+    private string currentText;
+
+    private bool active;
+
+    public void ActivateMonologue(bool condition, string message = "")
+    {
+        active = condition;
+        npcMessage.SetActive(active);
+
+        if(message != "")
+        {
+            StartCoroutine(ShowMessage(message));
+        }
+    }
+
+    private IEnumerator ShowMessage(string mes)
+    {
+        for (int i = 0; i < mes.Length; i++)
+        {
+            currentText = mes.Substring(0, i);
+            npcMessage.GetComponentInChildren<Text>().text = currentText;
+            yield return new WaitForSeconds(0.01f);
+
+            if(!active)
+            {
+                yield break;
+            }
+        }
+    }
+}
